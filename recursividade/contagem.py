1004cd6bab8252c1ef0ac5fbf1592cc1aee628e0
gentilpedro <gentil.pedro21@gmail.com>
2025-05-05 10:00:24 -0300

    ✅ Add new files in python

---
+++ recursividade/contagem.py
@@ -0,0 +1,26 @@
+# def contagem(n):
+#     if n == 0:
+#         print("Fim")
+#     else:
+#         print(n)
+#         contagem(n - 1)
+#
+# contagem(13)
+
+# def fatorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fatorial(n - 1)
+#
+# calculo = fatorial(5)
+# print(f"fatorial do 5: {calculo}")
+#
+# total = 1
+# for i in range(5,0 ,-1):
+#     total = total * i
+#
+#
+# print(f"fatorial do 5: {total}")
+
+
